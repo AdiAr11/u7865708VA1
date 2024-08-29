390468c13ed2df05fc3b65f635e7bf7ef6923ea5
AdiAr11 <aditya2000arora@gmail.com>
2024-08-30 00:14:00 +1000

    done

--- app/src/main/java/com/example/u7865708va1/MainActivity.java
+++ app/src/main/java/com/example/u7865708va1/MainActivity.java
@@ -3,15 +3,12 @@ package com.example.u7865708va1;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         new Thread(() -> {
             try {
                 URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");
-                JSONObject jsonResponse = getJsonObject(url);
+                JSONObject jsonResponse = getParsedJsonObject(url);
                 String title = jsonResponse.getString("title");
 
                 // Update the UI
@@ -44,20 +41,29 @@ public class MainActivity extends AppCompatActivity {
         }).start();
     }
 
-    private static JSONObject getJsonObject(URL url) throws IOException, JSONException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+    private  JSONObject getParsedJsonObject(URL url) {
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            response.append(line);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+
+            // Parse the JSON response and return
+            return new JSONObject(response.toString());
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
-        reader.close();
-
-        // Parse the JSON response
-        JSONObject jsonResponse = new JSONObject(response.toString());
-        return jsonResponse;
     }
 }
